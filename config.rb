be126463d9c2969b7ec35124846b7c0b4afc5705
franklin <franklin@weareinteractive.com>
2013-12-20 18:16:09 +0000

    cleanup and optimization

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@ activate :deploy do |deploy|
   deploy.clean  = true
   deploy.host   = "www.owl-stars.com"
   deploy.path   = "/var/www/owl-stars.com/htdocs"
-  deploy.flags  = "-avz --chmod=Dg+s,ug+w -e"
+  deploy.flags  = "-avz --chmod=Dg+s,ug+w,+r -e"
 end
 
 ###
@@ -53,11 +53,15 @@ end
 configure :build do
   # cache
   activate :minify_css
+  activate :minify_html
   activate :minify_javascript
+  activate :imageoptim do |options|
+    options.pngout_options    = false
+    options.advpng_options    = false
+  end
   # others
   activate :cache_buster
   activate :favicon_maker
-  activate :relative_assets
   # analytics
   activate :google_analytics do |ga|
     ga.anonymize_ip = true
